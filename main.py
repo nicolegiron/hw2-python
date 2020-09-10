f91b6dd7eecbc2acf80d5992ba3eff9b466e5e24
Nicole Giron <nqg5259@psu.edu>
2020-09-10 20:54:52 +0000

    final (hopefully)

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def getGradePoint(course):
 def run():
   grade = getGradePoint(input("Enter your course 1 letter grade: "))
   course1 = input("Enter your course 1 credit: ")
-  print(f"Grade point for course 1 is: {grade}HELSLDFJSDJKdsKJSD")
+  print(f"Grade point for course 1 is: {grade}")
 
   grade2 = getGradePoint(input("Enter your course 2 letter grade: "))
   course2 = input("Enter your course 2 credit: ")
